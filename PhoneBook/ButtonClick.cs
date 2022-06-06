c35fc95d7421379e82d99c12fd480455f546d215
Sali_dk52@itstepru.onmicrosoft.com <Sali_dk52@itstepru.onmicrosoft.com>
2022-06-06 16:23:04 +0300

    Добавьте файлы проекта.

---
+++ PhoneBook/ButtonClick.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PhoneBook
+{
+    class ButtonClick : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        Action _action;
+        public ButtonClick(Action action)
+        {
+            _action = action;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
---
+++ PhoneBook/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using MySql.Data.MySqlClient;
+using System.Windows.Input;
+
+namespace PhoneBook
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void Notify(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        Persone persone;
+        AuthorizationModel authorization;
+        public Persone MyPersone
+        {
+            get { return persone; }
+            set
+            {
+                persone = value;
+                Notify("MyPersone");
+            }
+
+        }
+        public MainViewModel()
+        {
+            authorization = new AuthorizationModel();
+            persone = new Persone();
+        }
+        public ICommand ShowWindow
+        {
+            get
+            {
+                return new ButtonClick
+                    (
+                    new Action(()=>
+                    {
+                    authorization.Open(persone);
+                    }));
+            }
+        }
+    }
+}
